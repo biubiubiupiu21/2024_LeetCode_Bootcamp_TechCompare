f0a2617b28fb7fed8de221b7520d9d6917657dc3
winniewu550 <wnwu.winnie2016@gmail.com>
2024-04-17 17:09:15 -0400

    update API

--- src/main/java/com/leetcode2024spring/ecommercedemo1/controller/ProductController.java
+++ src/main/java/com/leetcode2024spring/ecommercedemo1/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.leetcode2024spring.ecommercedemo1.controller;
 import com.leetcode2024spring.ecommercedemo1.collection.PriceHistory;
 import com.leetcode2024spring.ecommercedemo1.collection.Product;
 import com.leetcode2024spring.ecommercedemo1.collection.Review;
+import com.leetcode2024spring.ecommercedemo1.collection.User;
 import com.leetcode2024spring.ecommercedemo1.service.ProductServiceImp;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -36,6 +38,18 @@ public class ProductController {
         System.out.println(product.getCategory());
         return product;
     }
+
+
+    @GetMapping("/{productStringId}")
+    public ResponseEntity<Product> getProductByStringId(@PathVariable String productStringId) {
+        Product product = productService.getByProductStringId(productStringId);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 
     @GetMapping("/bycategory")
     public List<Product> getProductsByCategory(@RequestParam("category") String category) {
